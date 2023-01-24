7a2445d4acbbc20e9a08e902718711104e41f97c
den_minus <den.minus@mail.ru>
2023-01-24 11:30:08 +0300

    Условие про Машу

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,7 +2,6 @@
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
-Console.WriteLine(numberA+numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
 
